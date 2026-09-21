f754b7b9407a2681e57180d01804e260dff16d8a
Pavel Rosca <pavelroscawow@gmail.com>
2026-09-21 08:00:47 +0300

    Support EMAIL_USE_SSL for SMTP sending
    Aruba's mail server refuses outbound port 587 (STARTTLS) from the VPS but accepts 465 (SSL), which Django's SMTP backend needs EMAIL_USE_SSL for.

--- website-wag/config/settings.py
+++ website-wag/config/settings.py
@@ -249,6 +249,7 @@ if not DEBUG or PUBLIC_DEMO:
     EMAIL_HOST = config('EMAIL_HOST', default='')
     EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
     EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+    EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
     EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 else:
